33b95151bc82509f173fea625ebf46d92e3411dc
Tripxp1 <144802453+Tripxp1@users.noreply.github.com>
2026-04-22 11:14:08 +0300

    Update circle.h

--- include/circle.h
+++ include/circle.h
@@ -3,5 +3,20 @@
 #define INCLUDE_CIRCLE_H_
 #include <cstdint>
 
+class Circle {
+ private:
+  double radius;
+  double ference;
+  double area;
+
+ public:
+  explicit Circle(double rad);
+  void setRadius(double rad);
+  void setFerence(double fer);
+  void setArea(double ar);
+  double getRadius() const;
+  double getFerence() const;
+  double getArea() const;
+};
 
 #endif  // INCLUDE_CIRCLE_H_
