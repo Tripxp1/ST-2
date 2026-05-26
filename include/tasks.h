a7643eac3c9eb17b742f56781586f2e51c8c299b
Tripxp1 <144802453+Tripxp1@users.noreply.github.com>
2026-05-26 12:02:24 +0300

    Create tasks.h

---
+++ include/tasks.h
@@ -0,0 +1,7 @@
+// Copyright 2022 UNN-CS
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
+double earthTask();
+double poolTask(double rPool, double widthPath,
+                double priceConcrete, double priceFence);
+#endif  // INCLUDE_TASKS_H_
