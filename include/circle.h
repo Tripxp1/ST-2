a92324467f51a6926d4c0e6ee3426632f7919745
Леонид Парамонов <paramonov2005lol@gmail.com>
2026-06-03 14:52:22 +0300

    lab2

--- include/circle.h
+++ include/circle.h
@@ -2,7 +2,6 @@
 #ifndef INCLUDE_CIRCLE_H_
 #define INCLUDE_CIRCLE_H_
 
-<<<<<<< HEAD
 class Circle {
  private:
     double radius;
@@ -25,33 +24,6 @@ class Circle {
     double getRadius() const;
     double getFerence() const;
     double getArea() const;
-=======
-#include <cmath>
-#include <stdexcept>
-
-class Circle {
- private:
-    double radius_;
-    double circumference_;
-    double area_;
-
-    void updateFromRadius();
-    void updateFromCircumference(double circ);
-    void updateFromArea(double a);
-
- public:
-    explicit Circle(double r);
-    
-    void setRadius(double r);
-    void setCircumference(double c);
-    void setArea(double a);
-    
-    double getRadius() const;
-    double getCircumference() const;
-    double getArea() const;
-    
-    bool isValid() const;
->>>>>>> b6db93efe28c710d4340d209f52c57582f464387
 };
 
 #endif  // INCLUDE_CIRCLE_H_
--- src/circle.cpp
+++ src/circle.cpp
@@ -1,7 +1,6 @@
 // Copyright 2022 UNN-CS
 #include "circle.h"
 
-<<<<<<< HEAD
 #include <cmath>
 
 const double Circle::kPi = 3.14159265358979323846;
@@ -57,71 +56,3 @@ double Circle::getRadius() const { return radius; }
 double Circle::getFerence() const { return ference; }
 
 double Circle::getArea() const { return area; }
-=======
-const double PI = 3.14159265358979323846;
-
-Circle::Circle(double r) {
-    radius_ = 0.0;
-    circumference_ = 0.0;
-    area_ = 0.0;
-    setRadius(r);
-}
-
-void Circle::updateFromRadius() {
-    circumference_ = 2.0 * PI * radius_;
-    area_ = PI * radius_ * radius_;
-}
-
-void Circle::updateFromCircumference(double circ) {
-    if (circ < 0.0) {
-        circumference_ = 0.0;
-        radius_ = 0.0;
-        area_ = 0.0;
-    } else {
-        circumference_ = circ;
-        radius_ = circ / (2.0 * PI);
-        area_ = PI * radius_ * radius_;
-    }
-}
-
-void Circle::updateFromArea(double a) {
-    if (a < 0.0) {
-        area_ = 0.0;
-        radius_ = 0.0;
-        circumference_ = 0.0;
-    } else {
-        area_ = a;
-        radius_ = std::sqrt(a / PI);
-        circumference_ = 2.0 * PI * radius_;
-    }
-}
-
-void Circle::setRadius(double r) {
-    radius_ = r;
-    updateFromRadius();
-}
-
-void Circle::setCircumference(double c) {
-    updateFromCircumference(c);
-}
-
-void Circle::setArea(double a) {
-    updateFromArea(a);
-}
-
-double Circle::getRadius() const {
-    return radius_;
-}
-
-double Circle::getCircumference() const {
-    return circumference_;
-}
-
-double Circle::getArea() const {
-    return area_;
-}
-
-bool Circle::isValid() const {
-    return (radius_ >= 0.0 && circumference_ >= 0.0 && area_ >= 0.0);
-}
->>>>>>> b6db93efe28c710d4340d209f52c57582f464387
