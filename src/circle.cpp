84ae116729c047c2378884ad9901c468bcac1112
Tripxp1 <144802453+Tripxp1@users.noreply.github.com>
2026-06-03 12:04:04 +0300

    Update circle.cpp

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,24 +1,69 @@
 // Copyright 2022 UNN-CS
-
 #include "circle.h"
+
+const double PI = 3.14159265358979323846;
+
 Circle::Circle(double r) {
+    radius_ = 0.0;
+    circumference_ = 0.0;
+    area_ = 0.0;
     setRadius(r);
 }
+
+void Circle::updateFromRadius() {
+    circumference_ = 2.0 * PI * radius_;
+    area_ = PI * radius_ * radius_;
+}
+
+void Circle::updateFromCircumference(double circ) {
+    if (circ < 0.0) {
+        circumference_ = 0.0;
+        radius_ = 0.0;
+        area_ = 0.0;
+    } else {
+        circumference_ = circ;
+        radius_ = circ / (2.0 * PI);
+        area_ = PI * radius_ * radius_;
+    }
+}
+
+void Circle::updateFromArea(double a) {
+    if (a < 0.0) {
+        area_ = 0.0;
+        radius_ = 0.0;
+        circumference_ = 0.0;
+    } else {
+        area_ = a;
+        radius_ = std::sqrt(a / PI);
+        circumference_ = 2.0 * PI * radius_;
+    }
+}
+
 void Circle::setRadius(double r) {
-    radius = r;
-    ference = 2 * M_PI * radius;
-    area = M_PI * radius * radius;
+    radius_ = r;
+    updateFromRadius();
 }
-void Circle::setFerence(double f) {
-    ference = f;
-    radius = f / (2 * M_PI);
-    area = M_PI * radius * radius;
+
+void Circle::setCircumference(double c) {
+    updateFromCircumference(c);
 }
+
 void Circle::setArea(double a) {
-    area = a;
-    radius = sqrt(a / M_PI);
-    ference = 2 * M_PI * radius;
+    updateFromArea(a);
 }
-double Circle::getRadius() const { return radius; }
-double Circle::getFerence() const { return ference; }
-double Circle::getArea() const { return area; }
+
+double Circle::getRadius() const {
+    return radius_;
+}
+
+double Circle::getCircumference() const {
+    return circumference_;
+}
+
+double Circle::getArea() const {
+    return area_;
+}
+
+bool Circle::isValid() const {
+    return (radius_ >= 0.0 && circumference_ >= 0.0 && area_ >= 0.0);
+}
