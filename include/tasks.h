26ae47520f9e56786f8adc6c756809ce8b375c2c
Tripxp1 <144802453+Tripxp1@users.noreply.github.com>
2026-06-03 12:04:22 +0300

    Update tasks.h

--- include/tasks.h
+++ include/tasks.h
@@ -1,7 +1,11 @@
 // Copyright 2022 UNN-CS
 #ifndef INCLUDE_TASKS_H_
 #define INCLUDE_TASKS_H_
-double earthTask();
-double poolTask(double rPool, double widthPath,
-                double priceConcrete, double priceFence);
+
+double CalculateEarthGap();
+
+double CalculatePoolCost(double poolRadius, double pathWidth, 
+                         double concretePricePerSquareMeter, 
+                         double fencePricePerMeter);
+
 #endif  // INCLUDE_TASKS_H_
